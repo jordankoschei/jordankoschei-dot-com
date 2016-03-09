1183a19ca81a33d903bdac9fd90064eb240955da
Jordan Koschei <jordan@jordankoschei.com>
2016-03-09 09:57:16 -0500

    Added featured articles

--- wp-content/themes/pleiades/front-page.php
+++ wp-content/themes/pleiades/front-page.php
@@ -10,11 +10,12 @@
   </div>
 
   <?php
-  $recentPosts = get_posts('posts_per_page=6');
+  $featuredPosts = get_posts('posts_per_page=2');
+  $recentPosts = get_posts('posts_per_page=3');
   ?>
   <main class="container home-container">
     <?php
-    foreach( $recentPosts as $post ) :
+    foreach( $featuredPosts as $post ) :
     setup_postdata($post);
 
     $category = get_the_category();
@@ -26,10 +27,46 @@
     } else { $sourceNice = ''; }
     $url = get_post_meta(get_the_id(), 'url', true);
     ?>
+    <article class="home-container-block home-block-featured post-source-<?php echo $source; ?>">
+      <div class="home-block-content">
+        <span class="home-block-heading"><strong>Featured</strong> &bull; <?php echo get_the_date(); ?></span>
+        <?php if( ! get_post_meta(get_the_id(), 'url', true) ) : ?>
+        <h1 class="home-block-title"><a href="<?php the_permalink(); ?>" class="link--bordered link--background"><?php the_title(); ?></a></h1>
+        <?php else: ?>
+        <h1 class="home-block-title"><a href="<?php echo $url; ?>" class="link--bordered link--background" target="_blank"><?php the_title(); ?> <i>&rarr;</i></a></h1>
+        <?php endif; ?>
+
+        <?php the_excerpt(); ?>
+      </div>
+
+      <?php if( ! get_post_meta(get_the_id(), 'url', true) ) : ?>
+      <a href="<?php echo get_the_permalink(); ?>" class="post-more link--bordered link--background home-block-link">Read more&hellip;</a>
+      <?php else: ?>
+      <a href="<?php echo $url; ?>" target="_blank" class="post-more link--bordered link--background home-block-link">Read the article&rarr;</a>
+      <?php endif; ?>
+    </article>
+    <?php endforeach; ?>
+
+    <h2 class="home-container-subheading">Recent Articles</h2>
+
+    <div>
+    <?php
+    foreach( $recentPosts as $post ) :
+    setup_postdata($post);
+
+    $category = get_the_category();
+    $category = $category[0]->slug;
+
+    $source = get_post_meta(get_the_id(), 'source', true) ? get_post_meta(get_the_id(), 'source', true) : 'default';
+    if( $source != 'default' ) {
+      $sourceNice = get_post_meta(get_the_id(), 'sourceNice', true) ? get_post_meta(get_the_id(), 'sourceNice', true) . ' &bull; ' : ucfirst(get_post_meta(get_the_id(), 'source', true)) . ' &bull; ';
+    } else { $sourceNice = ''; }
+    $url = get_post_meta(get_the_id(), 'url', true);
+    ?>
     
     <article class="home-container-block home-category-<?php echo $category; ?> post-source-<?php echo $source; ?>">
       <div class="home-block-content">
-        <span class="home-block-heading"><?php echo get_the_date(); ?><?php echo $sourceNice; ?></span>
+        <span class="home-block-heading"><?php echo $sourceNice; ?><?php echo get_the_date(); ?></span>
         <?php if( ! get_post_meta(get_the_id(), 'url', true) ) : ?>
         <h1 class="home-block-title"><a href="<?php the_permalink(); ?>" class="link--bordered link--background"><?php the_title(); ?></a></h1>
         <?php else: ?>
@@ -47,6 +84,11 @@
     </article>
 
     <?php endforeach; ?>
+
+    <a href="<?php echo site_url(); ?>/writing" class="home-cta sub-cta">
+      <span>More writing</span>
+    </a>
+    </div>
   </main>
 
 <?php get_footer(); ?>
