0c7f7dbcfe37d2b4538daae2cdd4a8ddcd2964c1
Jordan Koschei <jordan@jordankoschei.com>
2016-03-09 15:36:14 -0500

    Got the newsletter set up.

--- wp-content/themes/pleiades/footer.php
+++ wp-content/themes/pleiades/footer.php
@@ -1,6 +1,21 @@
   <footer class="footer">
     <div class="inner">
-      <span class="copyright">&copy; 2015 Jordan Koschei</span>
+      <form class="newsletter" action="https://tinyletter.com/jordankoschei" method="post" target="popupwindow" onsubmit="window.open('https://tinyletter.com/jordankoschei', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true">
+        <div class="newsletter-content">
+          <h3 class="newsletter-title">Newsletter</h3>
+          <p class="newsletter-desc">A weekly(ish) list of things worth your attention from the worlds of design, technology, the arts, and more.</p>
+        </div>
+        <div class="newsletter-form">
+          <input class="newsletter-input" type="text" name="email" id="tlemail" placeholder="What's your email?">
+          <input type="hidden" value="1" name="embed">
+          <button class="newsletter-button" type="submit">Subscribe</button>
+        </div>
+      </form>
+      <div class="footer-bottom">
+        <span class="footer-copyright">&copy; 2015 Jordan Koschei</span>
+        <span class="footer-email"><a href='ma&#105;lto&#58;j&#111;&#114;%64&#37;61n&#64;jor%&#54;4&#97;n&#107;&#37;&#54;F%73%63hei&#46;co&#37;6&#68;'>jord&#97;n&#64;jordan&#107;o&#115;c&#104;&#101;i&#46;&#99;o&#109;</a></span>
+        <span class="footer-twitter"><a href="https://twitter.com/jordankoschei" target="_blank">@jordankoschei</a></span>
+      </div>
     </div>
   </footer>
 
